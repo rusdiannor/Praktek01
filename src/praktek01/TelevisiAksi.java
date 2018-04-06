069495e288abc9ee35dc04fa939517ad86bc9fe4
Rusdiannor <ryentnore2@gmail.com>
2018-04-06 18:24:01 +0800

    Instance Object

--- src/praktek01/TelevisiAksi.java
+++ src/praktek01/TelevisiAksi.java
@@ -10,5 +10,10 @@ package praktek01;
  * @author proWindows8.1
  */
 public class TelevisiAksi {
+    public static void main(String[] args) {
+        Televisi LG = new Televisi ();
+        Televisi Toshiba = new Televisi ();
+        Televisi Sharp = new Televisi ();
+    }
     
 }
