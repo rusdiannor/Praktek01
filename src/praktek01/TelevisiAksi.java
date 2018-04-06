a6278c9cb0f6093ef5d6dfb9caea78fd89465d4b
Rusdiannor <ryentnore2@gmail.com>
2018-04-06 18:40:32 +0800

    Mengisi nilai atribut

--- src/praktek01/TelevisiAksi.java
+++ src/praktek01/TelevisiAksi.java
@@ -14,6 +14,28 @@ public class TelevisiAksi {
         Televisi LG = new Televisi ();
         Televisi Toshiba = new Televisi ();
         Televisi Sharp = new Televisi ();
+        
+        LG.merk="LG";
+        LG.warna="Hitam";
+        LG.ukuran="32inc";
+        LG.jenis="Slim";
+        LG.kelebihan="Multemedia";
+        
+        Toshiba.merk="Toshiba";
+        Toshiba.warna="Putih";
+        Toshiba.ukuran="24inc";
+        Toshiba.jenis="Tabung";
+        Toshiba.kelebihan="Multemedia";
+        
+        Sharp.merk="Sharp";
+        Sharp.warna="Hitam";
+        Sharp.ukuran="32inc";
+        Sharp.jenis="Slim";
+        Sharp.kelebihan="HDMI";
+        
+        
+        
+   
     }
     
 }
