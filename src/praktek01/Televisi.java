94e9d84dac41858a150dd374ae28c36232b25ddb
Rusdiannor <ryentnore2@gmail.com>
2018-04-06 18:06:09 +0800

    Membuat Atribut dan Behavior

--- src/praktek01/Televisi.java
+++ src/praktek01/Televisi.java
@@ -10,5 +10,21 @@ package praktek01;
  * @author proWindows8.1
  */
 public class Televisi {
+    String merk;
+    String warna;
+    String ukuran;
+    String jenis;
+    String kelebihan;
     
+    void cetakInfo() {
+        System.out.println("Merk        :"+merk);
+        System.out.println("Warna       :"+warna);
+        System.out.println("Ukuran      :"+ukuran);
+        System.out.println("Jenis       :"+jenis);
+        System.out.println("Keunggulan  :"+kelebihan);
+        
+        System.out.println("");
+        System.out.println("Sebagai Hiburan");
+        System.out.println("");
+    }
 }
